033744c3927e60a6eb3d27ebcc82ee09332771da
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2025-08-25 15:18:14 +0700

    controller selesai

---
+++ app/controllers/About.php
@@ -0,0 +1,15 @@
+<?php
+
+class About
+{
+
+    public function index($nama = "Yossi Nordiansah", $profesi = "Programmer")
+    {
+        echo "hallo nama saya $nama, saya adalah seorang $profesi";
+    }
+
+    public function page()
+    {
+        echo "About/Page";
+    }
+}
