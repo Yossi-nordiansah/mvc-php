703e6286be8eda59450086f2e525943dc647acf9
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2025-08-28 14:44:30 +0700

    assets selesai

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,10 @@
-    <h1>ini adalah halaman Home</h1>
+    <div class="p-2 mb-2 bg-light rounded-3">
+        <div class="container-fluid">
+            <h1 class="display-5 fw-bold">Selamat Datang!</h1>
+            <img src="<?= BASEURL; ?>/img/pas-foto-almamater.jpg" width="200" alt="">
+            <p class="col-md-8 fs-4">
+                Ini adalah contoh jumbotron di Bootstrap 5, tapi dibuat dengan utility classes.
+            </p>
+            <button class="btn btn-primary btn-lg" type="button">Mulai</button>
+        </div>
+    </div>
